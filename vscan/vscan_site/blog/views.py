725e584e4fa219b75251c235ddd7ab601a41aafe
Nicholas Collins <nick.brian.collins@gmail.com>
2015-10-17 22:04:15 -0400

    can create post now

--- vscan/vscan_site/blog/views.py
+++ vscan/vscan_site/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import redirect
-from .models import Post 
+from django.shortcuts import get_object_or_404
+from .models import Post
+from django import forms
+from .forms import PostForm
+
 # Create your views here.
 
 def post_list(request):
@@ -19,4 +23,23 @@ def post_new(request):
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
